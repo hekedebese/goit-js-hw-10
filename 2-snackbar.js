a1e2f8471765073e9fd94bc739b7189bb49def22
hekedebese <137450756+hekedebese@users.noreply.github.com>
2026-04-23 21:02:34 +0000

    Deploying to gh-pages from @ hekedebese/goit-js-hw-10@2141a916e94f76afe7a304dceb0ba1aa7413f818 🚀

---
+++ 2-snackbar.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */import{i as o}from"./assets/vendor-BbbuE1sJ.js";const s=document.querySelector(".form");s.addEventListener("submit",n);function n(i){i.preventDefault();const t=s.elements.delay.value,m=s.elements.state.value;new Promise((e,r)=>{setTimeout(()=>{m==="fulfilled"?e(t):r(t)},t)}).then(e=>{o.success({message:`✅ Fulfilled promise in ${e}ms`})}).catch(e=>{o.error({message:`❌ Rejected promise in ${e}ms`})}),s.reset()}
+//# sourceMappingURL=2-snackbar.js.map
