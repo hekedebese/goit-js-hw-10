ffae1b70f000ec86714841f49c42e0a66cd6c1d3
Nikita Anohin <anohinnikita537@gmail.com>
2026-04-23 20:32:50 +0300

    js timer

---
+++ src/js/1-timer.js
@@ -0,0 +1,96 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const input = document.querySelector('#datetime-picker');
+const start = document.querySelector('button[data-start]');
+const days = document.querySelector('.value[data-days]');
+const hours = document.querySelector('.value[data-hours]');
+const minutes = document.querySelector('.value[data-minutes]');
+const seconds = document.querySelector('.value[data-seconds]');
+
+let userSelectedDate = null;
+let timerId = null;
+
+start.addEventListener('click', visibilityChange);
+start.disabled = true;
+
+function visibilityChange(event) {
+  event.target.disabled = true;
+  input.disabled = true;
+
+  if (timerId) {
+    clearInterval(timerId);
+  }
+
+  timer();
+
+  timerId = setInterval(timer, 1000);
+}
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+    const selectedDate = selectedDates[0];
+    const now = new Date();
+
+    if (selectedDate <= now) {
+      iziToast.error({
+        message: 'Please choose a date in the future',
+      });
+    } else {
+      userSelectedDate = selectedDate;
+      start.disabled = false;
+    }
+  },
+};
+
+flatpickr(input, options);
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
+
+function timer() {
+  const diff = userSelectedDate - Date.now();
+  const time = convertMs(diff);
+
+  days.textContent = addLeadingZero(time.days);
+  hours.textContent = addLeadingZero(time.hours);
+  minutes.textContent = addLeadingZero(time.minutes);
+  seconds.textContent = addLeadingZero(time.seconds);
+
+  if (diff <= 0) {
+    clearInterval(timerId);
+    input.disabled = false;
+
+    days.textContent = '00';
+    hours.textContent = '00';
+    minutes.textContent = '00';
+    seconds.textContent = '00';
+  }
+}
